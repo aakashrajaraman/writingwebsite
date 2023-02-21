c42f5d91b2a18bdb35ffa9e6e3340448952d975a
aakashrajaraman <20btrcl069@jainuniversity.ac.in>
2023-02-21 20:36:22 +0530

    created header

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,19 @@
-import react from 'react';
 
+import './header.css';
 const Header = () => {
-    return(<div class="header">
-    
+    return(
+    <div class="header">
+    <nav class="head1">
+        
+  <ul>
+    <li id = "title">Vexxed, By Aakash Rajaraman</li>
+    <li ><a href="#">Home</a></li>
+    <li ><a href="#">Short Stories</a></li>
+    <li ><a href="#">Poetry</a></li>
+    <li ><a href="#">About</a></li>
+  </ul>
+</nav>
+
     </div>
 );}
 
