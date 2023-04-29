a0b04e074f5c1a295c5b880e8eba19b3d643cfbb
aakashrajaraman <20btrcl069@jainuniversity.ac.in>
2023-04-29 19:15:58 +0530

    updated main page

--- src/pages/mainpage.jsx
+++ src/pages/mainpage.jsx
@@ -8,9 +8,19 @@ const Mainpage = (props) => {
         
         </div>
         <div class = "section">
-            <img src = "./prop.jpeg" style = {{width: "40%", height: "40%", borderRadius: "10%",
-        marginLeft:"30%"}} alt = "image" />
-            <p style={{marginLeft: "6%", paddingTop:"2%"}}>Propaganda</p>
+            <img src = "./prop.jpeg" style = {{width: "30%", height: "30%", borderRadius: "10%",
+        marginLeft:"30%", marginTop:"4%"}} alt = "image" />
+            <p style={{marginLeft: "6%", paddingTop:"2%"}}>
+                Hey! I'm Aakash.
+                <br/><br/>
+                I've been a writer ever since I was 12. Writing has always been a way for me to 
+                pour out the complex thunderstorm of thoughts in my head. I'm really excited to share my work with you!
+                <br/><br/>
+                I primarily write poetry and short stories, but occasionally I'll wade my way into non-fiction.
+                My work centers around existentialism, and what it means to be alive here and now. I hope you'll
+                enjoy and/or feel perplexed!
+                
+                </p>
         </div>
         
             <div class = "one">
