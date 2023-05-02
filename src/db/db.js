a2f425331651a97be3a721e99ddec52d84708c7f
aakashrajaraman <20btrcl069@jainuniversity.ac.in>
2023-05-02 09:20:20 +0530

    small changes

--- src/db/db.js
+++ src/db/db.js
@@ -1,3 +1,9 @@
 import { MongoClient } from 'mongodb';
+try{
+
 const url = "mongodb+srv://rajaramanaakash:Aakash5122@writingcluster.9r7ucj1.mongodb.net/?retryWrites=true&w=majority"
 const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+}catch(err){
+    console.log("!!!!!!!!!!!!!!",err);
+}
+export default client;
